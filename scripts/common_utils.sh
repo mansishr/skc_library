b674a28b2b9a872dd20b986d52c1c48291fba1e6
Raviraj Bhat <raviraj.bhat@intel.com>
2020-12-21 05:09:46 -0800

    Update epel release version to epel-8-10

--- scripts/common_utils.sh
+++ scripts/common_utils.sh
@@ -145,7 +145,7 @@ install_pre_requisites()
         then
 # RHEL
 
-	$PAC_INSTALLER install -y https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/e/epel-release-8-9.el8.noarch.rpm
+	$PAC_INSTALLER install -y https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/e/epel-release-8-10.el8.noarch.rpm
 	$PAC_INSTALLER install -y https://dl.fedoraproject.org/pub/fedora/linux/releases/32/Everything/x86_64/os/Packages/s/softhsm-2.5.0-4.fc32.3.x86_64.rpm
 	$PAC_INSTALLER install -y https://dl.fedoraproject.org/pub/fedora/linux/releases/32/Everything/x86_64/os/Packages/m/makeself-2.4.0-5.fc32.noarch.rpm
 	$PAC_INSTALLER install -y https://dl.fedoraproject.org/pub/fedora/linux/releases/32/Everything/x86_64/os/Packages/l/libgda-5.2.9-4.fc32.x86_64.rpm
