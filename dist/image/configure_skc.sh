618ae5a92278b383ffa391d11891e74c8a343e69
Dwivedi, Vartika <vartika.dwivedi@intel.com>
2021-02-24 03:44:51 -0800

    ISECL-14163: Added dockerfile to build skc-library docker container

--- dist/image/configure_skc.sh
+++ dist/image/configure_skc.sh
@@ -4,13 +4,20 @@ SKCLIB_INST_PATH=/opt/skc
 KMS_NPM_PATH=$SKCLIB_INST_PATH/etc/kms_npm.ini
 CREDENTIAL_PATH=$SKCLIB_INST_PATH/etc/credential_agent.ini
 
+echo "################ Install Admin user token....  #################"
+INSTALL_ADMIN_TOKEN=`curl --noproxy "*" -k -X POST https://$AAS_IP:8444/aas/token -d '{"username": "superadmin", "password": "superAdminPass" }'`
+if [ $? -ne 0 ]; then
+ echo "############ Could not get token for Install Admin User ####################"
+ exit 1
+fi
+
 update_credential_ini()
 {
 	sed -i "s|server=.*|server=https:\/\/$KBS_HOSTNAME:$KBS_PORT|g" $KMS_NPM_PATH
 	sed -i "s|request_params=.*|request_params=\"\/CN=$SKC_USER\"|g" $CREDENTIAL_PATH
 	sed -i "s|server=.*|server=$CMS_IP|g" $CREDENTIAL_PATH
 	sed -i "s|port=.*|port=$CMS_PORT|g" $CREDENTIAL_PATH
-	sed -i "s|^token=.*|token=\"$SKC_TOKEN\"|g" $CREDENTIAL_PATH
+	sed -i "s|^token=.*|token=\"$INSTALL_ADMIN_TOKEN\"|g" $CREDENTIAL_PATH	
 	curl -k -H 'Accept:application/x-pem-file' https://$CMS_IP:$CMS_PORT/cms/v1/ca-certificates > $SKCLIB_INST_PATH/store/cms-ca.cert
 }	
 
